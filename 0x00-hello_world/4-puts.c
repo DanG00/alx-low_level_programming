0401fdfa2a40189f24efb8825b47bd622eebfe58
Daniel Godwin <danielgodwin805@gmail.com>
2022-09-08 07:23:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+
+ *main- entry point
+
+ *
+
+ *Return: 0 (always)
+
+ */
+
+int main(void)
+  
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+
+  
+  return (0);
+  
+}
