44fdc164a987762051748284f6045a4831d19501
Daniel Godwin <danielgodwin805@gmail.com>
2022-09-13 06:36:28 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+  
+{
+  char puts[7] = "putchar"; 
+  int i;
+  int v = '_';
+ 
+  putchar(v); 
+
+  for (i = 0; i < 7; i++)
+  {     
+      putchar(puts[i]);    
+  }  
+  putchar('\n'); 
+  return (0);  
+}
